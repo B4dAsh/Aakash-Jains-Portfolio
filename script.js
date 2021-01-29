14ab8496e3efa75ff9016bff67cc98faf834052d
Ash Jain <aakashjainofficial@gmail.com>
2021-01-29 17:33:54 +0530

    Changed UI to Dark Mode

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const hamburger = document.getElementById("hamburger-menu")
+let hamburger;
 let menuOpen = false;
 
-function openHamburger() {
+function openHamburger(hamburger) {
 
     if (!menuOpen) {
+        hamburger = document.getElementById("hamburger-menu");
         hamburger.classList.add("menu-opened");
         menuOpen = true;
     } else {
+        hamburger = document.getElementById("hamburger-menu");
         hamburger.classList.remove("menu-opened");
+        document.getElementById("body").style.overflow = "initial";
         menuOpen = false;
     }
 
