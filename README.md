11a81398367ae8f5c65e01e69564b3ceb176b1b6
Ash Jain <aakashjainofficial@gmail.com>
2021-04-23 16:05:31 +0530

    Redesigned hb-menu, fixed bugs, refactored some code

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 * Find the website [here](https://aakashjain.ml).
 * The website is made using plain HTML and CSS without any frameworks, although not without a little Javascript here and there.
+* Deployed from and Hosted on [Netlify](https://netlify.com).
 * If you find any bugs or have some constructive criticism don't hesitate to reach out by [mail](mailto:aakashjainofficial@gmail.com) or by using links at the footer of the website.
 * Happy coding! 😊
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
-let hamburger;
+let hb;
 let menuOpen = false;
 
-function openHamburger(hamburger) {
-
+function hamburger(hamburger) {
+    
     if (!menuOpen) {
-        hamburger = document.getElementById("hamburger-menu");
-        hamburger.classList.add("menu-opened");
+        hb = document.getElementById("hamburger-menu-container");
+        hb.classList.add("menu-opened");
+        document.getElementById("hamburger-btn").innerHTML = "<i class=\"fas fa-times\"></i>"
         menuOpen = true;
     } else {
-        hamburger = document.getElementById("hamburger-menu");
-        hamburger.classList.remove("menu-opened");
+        hb = document.getElementById("hamburger-menu-container");
+        hb.classList.remove("menu-opened");
+        document.getElementById("hamburger-btn").innerHTML = "<i class=\"fas fa-bars\"></i>"
         menuOpen = false;
     }
 
