8dbcde5762e72c09f76c4ebba364e86ab210dc34
Ash Jain <aakashjainofficial@gmail.com>
2021-08-28 18:42:31 +0530

    added animations to header.

---
+++ scripts/anim.js
@@ -0,0 +1,18 @@
+let appear = [
+    ".header-main-text", ".header-main-btn" ]
+
+let up = [
+    ".navbar-title", ".hamburger-btn", ".navbar-links" ]
+
+let down = [
+    ".header-nav-container", ".header-mail-container" ]
+
+let left = [
+    ".expertise-container > .expertise-item:nth-of-type(2n+1)" ]
+
+let right = [
+    ".expertise-container > .expertise-item:nth-of-type(2n)" ]
+
+gsap.from(appear, {opacity: 0, duration: 1});
+gsap.from(up, {y: "-50vh", duration: 0.5});
+gsap.from(down, {y: "50vh", duration: 0.5});
